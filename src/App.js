83ff04e212270975abf13549b2fdde241146f4b5
Haseeb shah <haseebshah936@gmail.com>
2022-10-12 14:00:37 +0500

    setting

--- src/App.js
+++ src/App.js
@@ -14,24 +14,25 @@ import { CustomContextProvider } from "./Hooks/useCustomContext";
 import Ranking from "./pages/Ranking";
 import ContactUs from "./pages/ContactUs";
 import BuyerDashboard from "./pages/BuyerDashboard";
+import SellerGig from "./pages/SellerGig";
 
 function App(props) {
   const [open, setOpen] = useState(false);
   return (
-    // <CustomContextProvider value={{ open, setOpen }}>
-    //   <Router>
-    //     <Routes>
-    //       <Route path="/howitwork" element={<HowitWork />} />
-    //       <Route path="/about" element={<About />} />
-    //       <Route path="/contactus" element={<ContactUs />} />
-    //       <Route path="/signup" element={<Signup />} />
-    //       <Route path="/ranking" element={<Ranking />} />
-    //       <Route path="/home" element={<Landing />} />
-    //       <Route path="*" element={<Navigate to="/home" replace />} />
-    //     </Routes>
-    //   </Router>
-    // </CustomContextProvider>
-    <BuyerDashboard></BuyerDashboard>
+    <CustomContextProvider value={{ open, setOpen }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SellerGig />} />
+          <Route path="/howitwork" element={<HowitWork />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/ranking" element={<Ranking />} />
+          <Route path="/home" element={<Landing />} />
+          <Route path="*" element={<Navigate to="/home" replace />} />
+        </Routes>
+      </Router>
+    </CustomContextProvider>
   );
 }
 
