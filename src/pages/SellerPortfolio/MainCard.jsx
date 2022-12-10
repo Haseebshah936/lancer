729182b48b9a3733f686679197954a8e0f8973ad
Talha477737 <talha477737@gmail.com>
2022-12-10 16:38:14 +0500

    Cart Updates

---
+++ src/pages/SellerPortfolio/MainCard.jsx
@@ -0,0 +1,67 @@
+import { Paper, Typography } from "@mui/material";
+import { Box, styled } from "@mui/system";
+import React from "react";
+import colors from "../../utils/colors";
+
+export default function MainCard() {
+  return (
+    <>
+      <Paper
+        variant="outlined"
+        elevation={0}
+        sx={{
+          p: 3,
+          borderColor: "black",
+          borderWidth: "2px",
+          borderRadius: "10px",
+          "&:hover": {
+            outlineStyle: "solid",
+            OutlineColor: "black",
+            outlineWidth: "1px",
+          },
+        }}
+      >
+        <Header>
+          <Heading>Basic</Heading>
+          <Price>$900</Price>
+        </Header>
+        <Description>
+          <Name>Basic Web App</Name>I will develop an asp net mvc, dot net core
+          or blazor webapp
+        </Description>
+      </Paper>
+    </>
+  );
+}
+
+const Header = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  paddingBottom: "15px",
+});
+const Heading = styled(Typography)({
+  fontSize: "2.0rem",
+  fontWeight: "600",
+  color: colors.black,
+});
+
+const Description = styled(Typography)({
+  fontSize: "1.5rem",
+  fontWeight: "500",
+  color: colors.black,
+});
+
+const Price = styled(Typography)({
+  fontSize: "2.0rem",
+  fontWeight: "500",
+  color: colors.black,
+});
+
+const Name = styled(Typography)({
+  display: "inline",
+  fontSize: "1.5rem",
+  paddingRight: "3px",
+  fontWeight: "600",
+  color: colors.black,
+});
