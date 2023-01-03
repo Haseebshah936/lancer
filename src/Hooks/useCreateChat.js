a669306dde54f644fd9076f369bc5ff7d6f52f05
Haseeb shah <haseebshah936@gmail.com>
2023-01-03 22:03:44 +0500

    added function for group creation

--- src/Hooks/useCreateChat.js
+++ src/Hooks/useCreateChat.js
@@ -26,8 +26,29 @@ function useCreateChat(props) {
       });
   };
 
+  const createGroupChatRoom_Navigate = (creatorId, participantId) => {
+    /*
+     * This function is used to create a chat room and navigate to the chat page
+     * @param {String} creatorId - The id of the user who is creating the chat room => user._id
+     * @param {String} participantId - The id of the user who is the participant in the chatroom
+     */
+    requestMethod
+      .post("chatroom/createGroupChatroom", {
+        participants: [participantId],
+        creatorId,
+      })
+      .then((res) => {
+        navigate(`/chat`);
+      })
+      .catch((err) => {
+        console.log(err);
+        handleError(err);
+      });
+  };
+
   return {
     createChatRoom_Navigate,
+    createGroupChatRoom_Navigate,
   };
 }
 
