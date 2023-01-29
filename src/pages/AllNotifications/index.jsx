34714c392449558e10477fcdbcfc48174d6d3e37
Talha477737 <talha477737@gmail.com>
2023-01-30 03:43:51 +0500

    Minor Updates

---
+++ src/pages/AllNotifications/index.jsx
@@ -0,0 +1,12 @@
+import Footer from "../../components/Footer";
+import HeaderLoggedIn from "../../components/HeaderLoggedIn";
+
+export default function index() {
+  return (
+    <>
+      <HeaderLoggedIn />
+
+      <Footer />
+    </>
+  );
+}
